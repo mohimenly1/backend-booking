73c13d90568e8e14b7eece6ef797c302c13988b4
mohimenly1 <mohimentantoush@gmail.com>
2024-06-28 19:55:27 +0200

    new devs 6-28

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
+            'name' => 'nunllable|string|max:255',
             'phone' => ['required', 'string', 'unique:users', new PhoneNumber],
             'password' => 'required|string|min:8|confirmed',
         ]);
